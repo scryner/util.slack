17590c503c9c686e2cbd6120e93dc036b6826526
scryner <scryner@gmail.com>
2019-04-21 00:13:40 +0900

    implement slashcmd package

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/scryner/util.slack
 go 1.12
 
 require (
-	github.com/labstack/echo v3.3.10+incompatible // indirect
-	github.com/labstack/gommon v0.2.8 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.2.8
 	github.com/mattn/go-colorable v0.1.1 // indirect
 	github.com/mattn/go-isatty v0.0.7 // indirect
 	github.com/valyala/fasttemplate v1.0.1 // indirect
--- slashcmd/verify.go
+++ slashcmd/verify.go
@@ -1,1 +1,71 @@
 package slashcmd
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"time"
+)
+
+// Verifier verifies whether request was came from Slack
+type Verifier struct {
+	signingSecret   string
+	verifyTimestamp func(int64) bool
+}
+
+// Verify do verifying request
+func (v *Verifier) Verify(timestamp int64, reqSignature, reqBody string) error {
+	// verify timestamp
+	var ok bool
+
+	if v.verifyTimestamp == nil {
+		ok = defaultVerifyTimestamp(timestamp)
+	} else {
+		ok = v.verifyTimestamp(timestamp)
+	}
+
+	if !ok {
+		return errors.New("failed to verify timestamp")
+	}
+
+	// make sig_basestring
+	sigBaseString := fmt.Sprintf("v0:%d:%s", timestamp, reqBody)
+
+	// calculate my signature
+	hm := hmac.New(sha256.New, []byte(v.signingSecret))
+	hm.Write([]byte(sigBaseString))
+
+	mySignature := fmt.Sprintf("v0=%x", hm.Sum(nil))
+
+	// compare signature
+	ok = hmac.Equal([]byte(mySignature), []byte(reqSignature))
+	if !ok {
+		return errors.New("failed to compare signatures")
+	}
+
+	return nil
+}
+
+// NewVerifier to create verifier
+func NewVerifier(signingSecret string) *Verifier {
+	return &Verifier{
+		signingSecret:   signingSecret,
+		verifyTimestamp: defaultVerifyTimestamp,
+	}
+}
+
+func defaultVerifyTimestamp(timestamp int64) bool {
+	ti := time.Unix(timestamp, 0)
+	diff := int64(time.Now().Sub(ti))
+
+	if diff < 0 {
+		diff *= -1
+	}
+
+	if diff > int64(time.Minute*5) {
+		return false
+	}
+
+	return true
+}
